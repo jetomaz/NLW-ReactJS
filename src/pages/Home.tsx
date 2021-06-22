4cce3bf50d52cccb7fb3f7a8bb47a13082a99687
Jose Eduardo Tomaz <jetomaz@gmail.com>
2021-06-22 15:44:07 -0300

    estilizando a aplicação, tela de Home, Login

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import { placeholder } from '@babel/types';
+
+import '../styles/auth.scss';
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="ilustracao simbolizando perguntas e respostas" /> 
+                <strong>Crie salas de Q&amp;A</strong>
+                <p>Tire suas duvidas da audiencia em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="leatmeask"/>
+                    <button>
+                        <img src={googleIconImg} alt="logotipo do google"/>
+                        Crie sua Sala com o Google
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o Código da Sala"
+                        />
+                        <button type="submit" >
+                            Entrar na Sala
+                        </button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}

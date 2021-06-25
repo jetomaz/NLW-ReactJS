5ad1c2605ee1b8d8bdb33404b02a54a181388af0
Jose Eduardo Tomaz <jetomaz@gmail.com>
2021-06-25 15:48:10 -0300

    autenticação com o Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { createContext} from 'react';
+
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const TestContext = createContext('');
+
 function App() {
   return (
     <BrowserRouter>
+      <TestContext.Provider value={'Teste'}>
       <Route path="/" exact component={ Home }></Route>
       <Route path="/rooms/new" component={ NewRoom }></Route>
+      </TestContext.Provider>
     </BrowserRouter>
   );
 }
